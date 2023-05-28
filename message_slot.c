0d7278c8df85459a5f214ecbbf7f720aea3fb43c
eliavo <eliavo@mail.tau.ac.il>
2023-05-28 13:46:09 +0300

    progress20

--- message_slot.c
+++ message_slot.c
@@ -223,7 +223,7 @@ static ssize_t device_write( struct file*       file,
 {
   struct private_data* private_data = (struct private_data*)file->private_data;
   struct channel_list* current_channel = private_data->current_channel;
-  kprint("current channel: %p\n", current_channel);
+  printk("current channel: %p\n", current_channel);
   return -EINVAL;
   int write_index, i;
   char* message;
